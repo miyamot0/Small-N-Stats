f3b901abd7eb1091986f9da2d71befaf0bcaa0ca
Shawn Gilroy <ShawnPGilroy@gmail.com>
2016-03-17 21:25:12 -0400

    Initial Commit!!

---
+++ Small-N-Stats/Small-N-Stats/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Small_N_Stats
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
