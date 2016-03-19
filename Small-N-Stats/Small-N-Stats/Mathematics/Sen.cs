c11603850f98eae1eef531b735a55b63a96496d1
Shawn Gilroy <ShawnPGilroy@gmail.com>
2016-03-19 13:25:46 -0400

    Finalized Sen measures.

--- Small-N-Stats/Small-N-Stats/Mathematics/Sen.cs
+++ Small-N-Stats/Small-N-Stats/Mathematics/Sen.cs
@@ -19,92 +19,100 @@ namespace Small_N_Stats.Mathematics
 
         public SenSlopeModel ComputeTheilSen(List<double> data)
         {
-            List<double> data1 = data;
-            List<double> data2 = data;
+            List<double> data1 = new List<double>(data);
+
+            List<double> senSlopes = new List<double>();
 
             var N = data.Count;
-            //double[] ms = new double[N * (N - 1) / 2];
-            List<double> ms = new List<double>();
-            var pairs = 0;
+            var senPairs = 0;
 
-            double dx_sum = 0;
-            double dy_sum = 0;
-            var p = 0;
-            var n = 0;
-            var t = 0;
+            double xDeltaTotal = 0.0, yDeltaTotal = 0.0;
+            int p = 0, n = 0, t = 0;
 
             for (int i = 0; i < N - 1; i++)
             {
                 for (int j = i + 1; j < N; j++)
                 {
-                    var dy = (data[j] - data[i]);
-                    var dx = j - i;
-                    //ms[pairs] = dy / dx;
-                    ms.Add(dy / dx);
-                    pairs++;
-                    dx_sum += dx;
-                    dy_sum += dy;
+                    var yDelta = (data[j] - data[i]);
+                    var xDelta = j - i;
 
-                    if (dy > 0)
+                    senSlopes.Add(yDelta / xDelta);
+
+                    xDeltaTotal += xDelta;
+                    yDeltaTotal += yDelta;
+
+                    senPairs++;
+
+                    if (yDelta > 0)
+                    {
                         p++;
-                    else if (dy < 0)
+                    }
+                    else if (yDelta < 0)
+                    {
                         n++;
+                    }
                     else
+                    {
                         t++;
+                    }
                 }
             }
 
-            ms.Sort();
+            senSlopes.Sort();
 
-            double TS = med(ms);
-            double TS1 = TS;
-
-            double OLS = dy_sum / dx_sum;
+            double medianSenSlope = GetMedian(senSlopes);
             double S = (p - n);
             double POS = p;
-            double TAU = S / (pairs * 1.0);
-            double SDTau = System.Math.Sqrt((4 * N + 10) * 1.0 / (9 * N * N - 9 * N));
-            double Z = (TAU / SDTau);
-            double PVal = get2TailedPValue(System.Math.Abs(Z));
+            double TAU = S / ((double) senPairs);
 
-            double SDts = (SDTau / TAU) * TS;
+            /* Approximation of normal distribution (mean zero, no variance) */
+            double senVariance = (2.0 * (2.0 * N + 5)) / ((9 * N) * (N - 1));
+            double SDTau = System.Math.Sqrt(senVariance);
+
+            double senZScore = (TAU / SDTau);
+
+            double PVal = GetPValueFromUDistribution(System.Math.Abs(senZScore), true);
+
+            double SDts = (SDTau / TAU) * medianSenSlope;
             double VARts = SDts * SDts;
-            double PAIRS = pairs;
+            double PAIRS = senPairs;
 
             return new SenSlopeModel
             {
                 Observations = data,
                 Length = data.Count,
-                Sen = TS,
-                SenSD = (SDTau / TAU) * TS,
+                Sen = medianSenSlope,
+                SenSD = (SDTau / TAU) * medianSenSlope,
                 SenVariance = SDts * SDts,
-                LeastSquares = OLS,
-                Zscore = Z,
+                Zscore = senZScore,
                 Tau = TAU,
                 TauSD = SDTau,
                 P = PVal,
-                TS85 = new double[] { TS - 1.44 * SDts, TS + 1.44 * SDts },
-                TS90 = new double[] { TS - 1.645 * SDts, TS + 1.645 * SDts },
-                TS95 = new double[] { TS - 1.96 * SDts, TS + 1.96 * SDts }
+                TS85 = new double[] { medianSenSlope - 1.44 * SDts, medianSenSlope + 1.44 * SDts },
+                TS90 = new double[] { medianSenSlope - 1.645 * SDts, medianSenSlope + 1.645 * SDts },
+                TS95 = new double[] { medianSenSlope - 1.96 * SDts, medianSenSlope + 1.96 * SDts }
             };
         }
 
         public SenSlopeModel CompareTheilSen(SenSlopeModel bl, SenSlopeModel tx)
         {
-            SenSlopeModel copy = bl;
-            copy.SenSD = System.Math.Sqrt(bl.SenVariance + tx.SenVariance);
-            copy.Sen = bl.Sen - tx.Sen;
-            copy.Zscore = bl.Sen / System.Math.Sqrt(bl.SenVariance + tx.SenVariance);
-            copy.TS85 = bl.TS85;
-            copy.TS90 = bl.TS90;
-            copy.TS95 = bl.TS95;
+            SenSlopeModel senClone = bl;
+            senClone.SenSD = System.Math.Sqrt(bl.SenVariance + tx.SenVariance);
+            senClone.Sen = bl.Sen - tx.Sen;
+            senClone.Zscore = bl.Sen / System.Math.Sqrt(bl.SenVariance + tx.SenVariance);
+            senClone.TS85 = bl.TS85;
+            senClone.TS90 = bl.TS90;
+            senClone.TS95 = bl.TS95;
 
-
-            return copy;
+            return senClone;
         }
 
         public SenSlopeModel GetInterceptBaseline(SenSlopeModel current)
         {
+            /* Intercept derived based upon Conover's theorum */
+            /* Median(y) = SenSlope * Median(x) + Intercept */
+            /* Intercept = Median(y) - (SenSlope * Median(x)) */
+
             List<double> mx = new List<double>();
             for (int i = 0; i < current.Length; i++)
             {
@@ -116,8 +124,8 @@ namespace Small_N_Stats.Mathematics
             mx.Sort();
             my.Sort();
 
-            current.MedianX = med(mx);
-            current.MedianY = med(my);
+            current.MedianX = GetMedian(mx);
+            current.MedianY = GetMedian(my);
             current.ConoverIntercept = current.MedianY - current.Sen * current.MedianX;
 
             return current;
@@ -125,7 +133,12 @@ namespace Small_N_Stats.Mathematics
 
         public SenSlopeModel GetInterceptTreatment(SenSlopeModel current, int prevLength)
         {
+            /* Intercept derived based upon Conover's theorum */
+            /* Median(y) = SenSlope * Median(x) + Intercept */
+            /* Intercept = Median(y) - (SenSlope * Median(x)) */
+
             List<double> mx = new List<double>();
+
             for (int i = prevLength; i < current.Length + prevLength; i++)
             {
                 mx.Add(i + 1);
@@ -136,27 +149,68 @@ namespace Small_N_Stats.Mathematics
             mx.Sort();
             my.Sort();
 
-            current.MedianX = med(mx);
-            current.MedianY = med(my);
+            current.MedianX = GetMedian(mx);
+            current.MedianY = GetMedian(my);
             current.ConoverIntercept = current.MedianY - current.Sen * current.MedianX;
 
             return current;
         }
 
-        private double med(List<double> data)
+        private double GetMedian(List<double> data)
         {
-            List<double> copy = new List<double>(data);
-
-            int mid = (int)System.Math.Floor(copy.Count / 2.0);
-            return (copy[mid] + copy[mid - (1 - copy.Count % 2)]) / 2.0;
+            List<double> listClone = new List<double>(data);
+            int midItemRoundedDown = (int) System.Math.Floor(listClone.Count / (double) 2);
+            return (listClone[midItemRoundedDown] + listClone[midItemRoundedDown - (1 - listClone.Count % 2)]) / (double) 2;
         }
 
-        public double get2TailedPValue(double z)
+        public double GetPValueFromUDistribution(double x, bool isTwoTailed)
         {
-            z = System.Math.Abs(z);
-            var p2 = (((((.000005383 * z + .0000488906) * z + .0000380036) * z + .0032776263) * z + .0211410061) * z + .049867347) * z + 1;
-            p2 = System.Math.Pow(p2, -16);
-            return p2;
+            /* ORIGINAL AUTHOR
+            * 
+            * Ben Tilly <btilly@gmail.com>
+            * 
+            * Originl Perl version by Michael Kospach <mike.perl@gmx.at>
+            * 
+            * Nice formating, simplification and bug repair by Matthias Trautner Kromann
+            * <mtk@id.cbs.dk>
+            * 
+            * COPYRIGHT 
+            * 
+            * Copyright 2008 Ben Tilly.
+            * 
+            * This library is free software; you can redistribute it and/or modify it
+            * under the same terms as Perl itself.  This means under either the Perl
+            * Artistic License or the GPL v1 or later.
+            */
+
+            double p = 0;
+            var absx = System.Math.Abs(x);
+
+            if (absx < 1.9)
+            {
+                p = System.Math.Pow((1 + absx * (.049867347 + absx * (.0211410061 + absx * (.0032776263 + absx * (.0000380036 + absx * (.0000488906 + absx * .000005383)))))), -16) / (double)2;
+            }
+            else if (absx <= 100)
+            {
+                for (int i = 18; i >= 1; i--)
+                {
+                    p = i / (absx + p);
+                }
+
+                p = System.Math.Exp(-.5 * absx * absx) / System.Math.Sqrt(2 * System.Math.PI) / (absx + p);
+            }
+
+            if (x < 0)
+            {
+                p = 1 - p;
+            }
+
+            if (isTwoTailed)
+            {
+                p = p * 2;
+            }
+
+            return p;
         }
     }
 }
